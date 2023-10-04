d618d344c10a436ccc80508c01a0035b1784e051
Piotr Liszka <piotr.liszka.pl@gmail.com>
2023-10-04 06:07:35 +0200

    31. DI

---
+++ app/src/main/java/pl/maniak/fooddataviewer/di/DependencyInjection.kt
@@ -0,0 +1,44 @@
+package pl.maniak.fooddataviewer.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import dagger.multibindings.IntoMap
+import pl.maniak.fooddataviewer.foodlist.FoodListViewModel
+import javax.inject.Provider
+import javax.inject.Singleton
+import kotlin.reflect.KClass
+
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
+
+@Component(modules = [ApplicationModule::class, ViewModelModule::class])
+interface ApplicationComponent {
+
+    fun viewModelFactory(): ViewModelProvider.Factory
+}
+
+@Module
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun viewModels(providers: MutableMap<Class<out ViewModel>, Provider<ViewModel>>): ViewModelProvider.Factory {
+        return ViewModelFactory(providers)
+    }
+}
+
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(FoodListViewModel::class)
+    abstract fun foodListViewModel(viewModel: FoodListViewModel): FoodListViewModel
+}
