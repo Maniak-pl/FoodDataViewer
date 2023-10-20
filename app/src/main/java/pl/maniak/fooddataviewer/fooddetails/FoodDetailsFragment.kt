f2cf2e9fbddddf62d2f1ac6203a5dffefe43b10c
Piotr Liszka <piotr.liszka.pl@gmail.com>
2023-10-20 06:12:01 +0200

    55. Scan Layout

--- app/src/main/java/pl/maniak/fooddataviewer/fooddetails/FoodDetailsFragment.kt
+++ app/src/main/java/pl/maniak/fooddataviewer/fooddetails/FoodDetailsFragment.kt
@@ -4,9 +4,10 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
+import pl.maniak.fooddataviewer.R
 import pl.maniak.fooddataviewer.getViewModel
 
-class FoodDetailsFragment : Fragment() {
+class FoodDetailsFragment : Fragment(R.layout.food_details_fragment) {
 
     private val args: FoodDetailsFragmentArgs by navArgs()
     private lateinit var disposable: Disposable
